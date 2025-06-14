abfca6004038ff7e3105152d53d6eb8f92688945
kalin balabnov <52208012+kalinski10@users.noreply.github.com>
2025-06-14 18:37:04 +0100

    Initial Commit

---
+++ DemoCodeVibingLogin/DemoCodeVibingLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCodeVibingLoginApp.swift
+//  DemoCodeVibingLogin
+//
+//  Created by Kalin Balabanov on 14/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct DemoCodeVibingLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
